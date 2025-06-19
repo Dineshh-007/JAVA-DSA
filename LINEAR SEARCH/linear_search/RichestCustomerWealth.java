ba104399bb93a056e2f96834772e0b5426eff261
Dineshh-007 <dineshedine007@gmail.com>
2025-06-19 20:00:54 +0530

    RichestCustomerWealth.java

--- LINEAR SEARCH/linear_search/RichestCustomerWealth.java
+++ LINEAR SEARCH/linear_search/RichestCustomerWealth.java
@@ -1,4 +1,17 @@
-package arrays;
+package linear_search;
 
 public class RichestCustomerWealth {
+    static int richest(int[][] arr){
+        int count = Integer.MIN_VALUE;
+        for(int row = 0 ; row < arr.length ; row++){
+            int sum = 0;
+            for(int col = 0 ; col < arr[row].length ; col++ ){
+                sum += arr[row][col];
+            }
+            if(count < sum ){
+                count = sum;
+            }
+        }
+        return count;
+    }
 }
