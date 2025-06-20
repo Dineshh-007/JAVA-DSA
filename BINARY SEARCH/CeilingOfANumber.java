42ca864dd11beb8fec0579652a94569390ebce73
Dineshh-007 <dineshedine007@gmail.com>
2025-06-20 19:55:33 +0530

    Added CeilingOfANumber.java

---
+++ BINARY SEARCH/CeilingOfANumber.java
@@ -0,0 +1,28 @@
+
+public class CeilingOfANumber {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 17;
+        int ans = search(arr , target );
+        System.out.println(ans);
+    }
+    static int search(int[] arr , int target){
+        int start = 0;
+        int end = arr.length-1;
+        while( start <= end){
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] < target){
+                start = mid+1;
+            }
+            else if (arr[mid] > target) {
+                end = mid -1;
+
+            }
+            else if  (arr[mid] == target){
+                return mid;
+            }
+        }
+        return arr[start];
+    }
+}
